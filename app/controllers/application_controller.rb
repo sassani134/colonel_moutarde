b213ad1f3286f8684c1b9d8862fffd82e4e98af1
Hai-Dang <haidang.hathuc@gmail.com>
2018-12-03 17:57:56 +0100

    modals sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+	added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+	devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+	devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
