8c1fd90fd1b61188ac8ae521455d698dea531191
guillaume <elros.du.13@gmail.com>
2018-12-04 11:33:00 +0100

    first choice

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,17 @@
 class UserController < ApplicationController
   def index
-    
+    @copies = current_user.copies
   end
 
   def add_game
-
+    @games = Game.all
+    @title=[]
+    @games.each do |game|
+      @title << game.title
+    end
   end
 
+  def save_game
+  end
 end
 
