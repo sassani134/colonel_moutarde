92233f361f9926001ea2426a0bf1a45ca515f829
RomainRec <romain.recher@gmail.com>
2018-12-05 13:16:27 +0100

    map et page show ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,4 @@
 //= require turbolinks
 //= require active
 //= require_tree .
+
---
+++ app/assets/javascripts/others/map.js
@@ -0,0 +1,35 @@
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(48.8566, 2.3522),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+}
+function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=ENV['GOOGLE_MAPS_API_KEY']callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
