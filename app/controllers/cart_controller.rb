72346cd0702b91baf0e2aeb489b9ebed672f87dd
guillaume <elros.du.13@gmail.com>
2018-12-05 13:18:02 +0100

    view basique cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,4 @@
 class CartController < ApplicationController
-
   
   respond_to? :html, :js
  
@@ -11,97 +10,33 @@ class CartController < ApplicationController
       else
         @cart = Cart.create!(user_id: current_user.id)
       end
-      @content = @cart.items
-      @price = 0
-      @content.each_with_index do |content, index| 
-        @price += content.price * @cart.quantities[index].to_i
-      end
-      @price
+      @content = @cart.copies
+      @price = 3.5 * @content.count
     else
     end
-  end
-
-  def create
-    if user_signed_in?
-      @cart = Cart.where(:user_id => current_user.id)[0]
-      unless @cart.item_ids.include?(params[:id_add].to_i)
-        @new_cart = @cart.item_ids << params[:id_add].to_i
-        Cart.where(:user_id => current_user.id)[0].item_ids = @new_cart
-        @new_cart = Cart.where(:user_id => current_user.id)[0].item_ids
-        @order = @new_cart.index(params[:id_add].to_i)
-        @cart.quantities.insert(@order, 1)
-        @cart.save
-        redirect_to "/product"
-        flash[:success] = "You successfully added the product to your cart"
-      else
-        @quant = @cart.quantities[@cart.item_ids.index(params[:id_add].to_i)].to_i
-        @quant += 1
-        @cart.quantities[@cart.item_ids.index(params[:id_add].to_i)] = @quant.to_s
-        @cart.save
-        redirect_to "/product" 
-        flash[:alert] = "You successfully added the product to your cart"
-      end
-    else
-      redirect_to "/product" 
-      flash[:alert] = "You must be connected to have a cart"
-    end
-  end
-
-  def update
-    @cart = Cart.find(params[:id])
-    @array = @cart.item_ids
-    @ind = @cart.item_ids.index(params[:id_modify].to_i)
-    if params[:increase] == "true"
-      @cart.quantities[@ind] = @cart.quantities[@ind].to_i + 1
-      @cart.save
-    else
-      @cart.quantities[@ind] = @cart.quantities[@ind].to_i - 1
-      if @cart.quantities[@ind] == 0
-        @cart.quantities[@ind] = @cart.quantities[@ind].to_i + 1
-        @cart.save
-      else
-        @cart.save
-      end
-    end
-    @content = @cart.items
-    @price = 0
-    @content.each_with_index do |content, index| 
-      @price += content.price * @cart.quantities[index].to_i
-    end
-    @price
   end
 
   def destroy
     @cart = Cart.find(params[:id])
-    @array = @cart.item_ids
-    @ind = @cart.item_ids.index(params[:id_delete].to_i)
+    @array = @cart.copy_ids
+    @ind = @cart.copy_ids.index(params[:id_delete].to_i)
     @array.delete(params[:id_delete].to_i)
-    @cart.quantities.delete_at(@ind)
-    @cart.save
-    @cart.item_ids = @array
-    @content = @cart.items
-    @price = 0
-    @content.each_with_index do |content, index| 
-      @price += content.price * @cart.quantities[index].to_i
-    end
-    @price
+    @cart.copy_ids = @array
+    @content = @cart.copies
+    @price = 3.5 * @content.count
+  
   end
   
   def checkout
     if user_signed_in?
       @cart = Cart.where(:user_id => current_user.id)[0]
-      @content = @cart.items
-      @price = 0
-      @content.each_with_index do |content, index| 
-        @price += content.price * @cart.quantities[index].to_i
-      end
-      if @cart.quantities != []
-        @order = Order.create!(user: @cart.user, quantities: @cart.quantities, price: @price)
-        @order.item_ids = @cart.item_ids
-        @cart.quantities = []
-        @cart.save
-        Cart.where(:user_id => current_user.id)[0].item_ids = []
-        redirect_to "/product"
+      @content = @cart.copies
+      @price = 3.5 * @content.count
+      if @cart.copy_ids != []
+        @order = Order.create!(user: @cart.user, price: @price)
+        @order.copy_ids = @cart.copy_ids
+        Cart.where(:user_id => current_user.id)[0].copy_ids = []
+        redirect_to "/"
         flash[:success] = "Vous avez réussi à passer votre commande. Retrouvez là dans votre onglet commande :)"
       else
         redirect_to "/cart"
@@ -111,11 +46,9 @@ class CartController < ApplicationController
   end
 
   def empty
-    Cart.where(:user_id => current_user.id)[0].item_ids = []
-    @cart = Cart.where(:user_id => current_user.id)[0]
-    @cart.quantities = []
-    @cart.save
+    Cart.where(:user_id => current_user.id)[0].copy_ids = []
     redirect_to "/product"
-    flash[:success] = "Votre panier a été vidé, retrouvez maintenant d'autres photos qui pourraient vous plaire"
+    flash[:success] = "Votre panier a été vidé, retrouvez maintenant d'autres jeux qui pourraient vous plaire"
   end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'order/index'
-  get 'order/destroy'
+  resources :cart
   devise_for :users
   get 'home/index'
   root to: "home#index"
@@ -10,5 +9,6 @@ Rails.application.routes.draw do
   get '/dashboard', to: "user#index"
   get '/addgame', to: "user#add_game"
   resources :charges
+  get '/cart', to: "cart#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
