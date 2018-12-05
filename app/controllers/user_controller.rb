0649e5baa50a8145c7a4255516087f47d9368806
guillaume <elros.du.13@gmail.com>
2018-12-05 14:54:18 +0100

    game rented

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,12 @@
 class UserController < ApplicationController
   def index
-    @copies = current_user.copies
+    @rented_copies = current_user.copies.where(:rented => false)
+    #@confirmed_copies = @rented_copies.orders.where(:rented => false)
+    #@unconfirmed_copies = @rented_copies.where(:rented => false)
+    @renting_copies = current_user.copies.where(:rented => true)
+    @cart=Cart.where(:user_id => current_user.id)
+    @orders=current_user.orders
+    
   end
 
   def add_game
