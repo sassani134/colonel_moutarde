b41394c90b44ba5b6d87bc962b098723f9a72e1c
guillaume <elros.du.13@gmail.com>
2018-12-07 10:31:00 +0100

    routes add

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get '/contact', to: "home#contact"
   get '/explore', to: "home#explore"
   get '/dashboard', to: "dashboard#index"
-  get '/add_game', to: "dashboard#add_game"
-  post '/add_game', to: "dashboard#save_game"
+  get '/add', to: "dashboard#add_game"
+  post '/add', to: "dashboard#save_game"
   resources :charges
   resources :listing
+  get '/add_game', to: "add_game#new"
+  
   get '/cart', to: "cart#index"
   get '/show_orders/:id', to: 'dashboard#toggle_past'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
