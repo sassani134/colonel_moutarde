35687fd07943c048cb217269115d9534871d775c
guillaume <elros.du.13@gmail.com>
2018-12-10 18:05:01 +0100

    add tel to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_193627) do
+ActiveRecord::Schema.define(version: 2018_12_10_170359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(version: 2018_12_07_193627) do
     t.integer "role"
     t.string "username"
     t.string "address"
+    t.string "telephone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
