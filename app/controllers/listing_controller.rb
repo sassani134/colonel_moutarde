33e81140c23707395f54b5d4cfb8bf841e11c94f
RomainRec <romain.recher@gmail.com>
2018-12-11 16:25:09 +0100

    test

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -21,6 +21,7 @@ class ListingController < ApplicationController
     @game= Game.find(params[:id])
     if user_signed_in?
       @copies = @game.copies.where.not(:user_id => current_user.id)
+      @cart=Cart.where(:user_id => current_user.id)[0]
     else
       @copies = @game.copies
     end
@@ -30,15 +31,11 @@ class ListingController < ApplicationController
     @long = []
     @lat = []
     @proprio = []
-    @copy = []
     @copies.each do |copy|
     	@lat << copy.latitude
     	@long << copy.longitude
       @proprio << copy.user.username
       @copy = copy.id.to_s
-    end
-    if user_signed_in?
-      @cart=Cart.where(:user_id => current_user.id)[0]
     end
   end
 
