50e09a806210916568126e893274ce19c9c2a387
guillaume <elros.du.13@gmail.com>
2018-12-12 18:01:06 +0100

    reverse bullshit"

--- db/migrate/20181203101411_create_games.rb
+++ db/migrate/20181203101411_create_games.rb
@@ -3,9 +3,6 @@ class CreateGames < ActiveRecord::Migration[5.2]
     create_table :games do |t|
       t.string :title
       t.text :description
-      t.string :code
-      t.string :cate
-      t.string :image_url
       t.timestamps
     end
   end
